d5992b446c9136c3ce3455ceac02d976f74e8a07
Miller Oleg <miller.oleg@gmail.com>
2020-10-19 16:58:08 +0300

    Update api_calls.py
    1. create get_saved_stories method
    2. update to have a persistent session

--- api_calls.py
+++ api_calls.py
@@ -9,6 +9,7 @@ class Newsblur_fetcher(object):
  def __init__(cls):
     with open(config_path) as ymlfile:
         cls.config = yaml.load(ymlfile)
+    cls.connection_session = requests.Session()
      
  def login(self):
   '''
@@ -16,11 +17,25 @@ class Newsblur_fetcher(object):
   '''
   extended_url = '/api/login'  
   payload = {'username': self.config['user_name'],'password' : self.config['password'].encode('utf-8')}
-  response = requests.post(self.config['URL']+extended_url, data=payload,verify=False)
+  response = self.connection_session.post(self.config['URL']+extended_url, data=payload,verify=False)
   if response.status_code==200 and json.loads(newblur_login.content.decode('utf-8').replace("'", '"'))['authenticated']==True:
    return True
   elif esponse.status_code!=200:
    return('Response code is not 200')
   elif json.loads(newblur_login.content.decode('utf-8').replace("'", '"'))['authenticated']!=True:
    return('Authentication Failed:'  + str(json.loads(newblur_login.content.decode('utf-8').replace("'", '"'))['errors']))
+  
+ def get_saved_stories(self):
+  extended_url='reader/starred_stories?page='
+  page_index = 1
+  stories_page=self.connection_session.get(self.config['URL'] + extended_url+str(page_index),verify=False)
+  while len(stories_page.content.decode('utf-8').replace("'", '"'))['stories'])>0:
+   stories_page=self.connection_session.get(self.config['URL'] + extended_url+str(page_index),verify=False)
+   if stories_page.status_code!=200:
+    return("Bad response")
+   if stories_page.content.decode('utf-8').replace("'", '"'))['stories'] is None:
+    return("Page : " + str(page_index) + " returned no stories")
+   #TODO parse the stories and write them to a CSV file
+   page_index+=1
+   
 

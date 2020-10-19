46099d0a0f818c2343ac5986199237154493344c
Miller Oleg <miller.oleg@gmail.com>
2020-10-19 15:34:04 +0300

    Update api_calls.py
    Login method - complete

--- api_calls.py
+++ api_calls.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 config_path = "config.yaml" 
 
@@ -8,4 +9,21 @@ class Newsblur_fetcher(object):
  def __init__(cls):
     with open(config_path) as ymlfile:
         cls.config = yaml.load(ymlfile)
+     
+ def login(self):
+  '''
+  performs a log-in action to initiate a session with the s
+  '''
+  base_url = self.config['']
+  username = self.config['']
+  password = self.config['']
+  extended_url = '/api/login'  
+  payload = {'username': self.config[''],'password' : self.config['']}
+  response = requests.post(self.config['URL']+extended_url, data=payload,verify=False)
+  if response.status_code==200 and json.loads(newblur_login.content.decode('utf-8').replace("'", '"'))['authenticated']==True:
+   return True
+  elif esponse.status_code!=200:
+   return('Response code is not 200')
+  elif json.loads(newblur_login.content.decode('utf-8').replace("'", '"'))['authenticated']!=True:
+   return('Authentication Failed:'  + str(json.loads(newblur_login.content.decode('utf-8').replace("'", '"'))['errors']))
 

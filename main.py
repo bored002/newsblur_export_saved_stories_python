6b8b6a6430261823dee2de59cae48e422a5fc5eb
Miller Oleg <miller.oleg@gmail.com>
2020-12-24 11:20:17 +0200

    Main module
    Adding imported file to run

---
+++ main.py
@@ -0,0 +1,14 @@
+import time
+def test_module():
+    print("This is a Test.")
+    print("waiting...")
+    timer = 30
+    while timer>0:
+        print('Wait Time left : ' + str(timer))
+        time.sleep(10)
+        timer-=10        
+    print("Done waiting.")
+	
+if __name__ == "__main__":
+	test_module()
+ #Place Holder

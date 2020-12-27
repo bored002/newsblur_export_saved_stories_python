2d28910795594e69607091d92a2278698d1eced5
Miller Oleg <miller.oleg@gmail.com>
2020-12-27 12:13:41 +0200

    Create parser.py
    Skeleton for parsing

---
+++ src/parser.py
@@ -0,0 +1,18 @@
+import json
+
+import logging
+
+config_path = "config.yaml" 
+
+class Story_Parser(object):
+  @classmethod
+  def __init__(cls):
+    '''
+    Place Holder
+    '''
+    
+ @classmethod
+ def teardown(cls):
+  '''
+  Best practices
+  '''

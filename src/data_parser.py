d4b20bb6e944738c27a02cbd79bf17c9c4561b86
bored002 <miller.oleg@gmail.com>
2020-12-27 14:01:24 +0200

    Improving structure and code

---
+++ src/data_parser.py
@@ -0,0 +1,30 @@
+import json
+
+import logging
+
+config_path = "config.yaml" 
+
+class Story_Parser(object):
+  @classmethod
+  def __init__(cls):
+    '''
+    Place Holder
+    '''
+ 
+  def parse_stories(self, fresh_story_list):
+    '''
+    Parse stories froma pulled page 
+    from each story exctract the following:
+    1. Origin : [Lifehacker]
+    2. Title: 
+    3. Date
+    4. Link to story
+
+    each story will be a dict : {origin: , title, link, date , tags}
+    '''
+    
+  @classmethod
+  def teardown(cls):
+    '''
+    Best practices
+    '''

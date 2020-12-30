4ea7247128c0ef66253910e2d9781dcce4fe2157
Miller Oleg <miller.oleg@gmail.com>
2020-12-30 13:38:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def test_module():
     print("Done waiting.")
 	
 if __name__ == "__main__":
-    newsblur_object = api_caller(None,None)
+    newsblur_object = api_calls.api_caller(None,None)
     if newsblur_object.login_newsblur() == True:
       x = newsblur_object.get_feeds()
       y = newsblur_object.get_saved_stories()

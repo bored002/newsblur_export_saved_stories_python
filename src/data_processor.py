9fc1150c67f156ea93fb88705909c813081cac33
bored002 <miller.oleg@gmail.com>
2021-01-17 11:22:49 +0200

    Adding skeleton methods for data sciences.

--- src/data_processor.py
+++ src/data_processor.py
@@ -5,5 +5,27 @@ import matplotlib
 class data_science(object):
     '''
     This module will handle various data analysis actions
-    
+
     '''
+
+    @classmethod
+    def __init__(cls):
+        '''
+        Template
+        '''
+
+    def analyze_origin_distribution():
+        '''
+        Generates a Data Frame which conaints the story distribution per origin website (feed)
+        '''
+
+    def analyze_trends():
+        '''
+        Analyze the trends for each feed:  +/- from last sample
+        '''
+
+    @classmethod
+    def teardown_class(cls):
+        '''
+        Template
+        '''

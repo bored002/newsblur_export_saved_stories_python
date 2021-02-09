84b13ed139cef083f01405d3738aa33d158b0760
bored002 <miller.oleg@gmail.com>
2021-02-09 11:48:06 +0200

    Adding argument handling

--- main.py
+++ main.py
@@ -14,14 +14,21 @@ def test_module():
     print("Done waiting.")
 	
 if __name__ == "__main__":
-    print(f"Arguments count: {len(sys.argv)}")
-    for i, arg in enumerate(sys.argv):
-        print(f"Argument {i:>6}: {arg}") # should pass newsblur user,password and google api key
-    newsblur_object = api_calls.api_caller(None,None)
+    print(f"Arguments count: {len(sys.argv)}") #debug
+    # for i, arg in enumerate(sys.argv):
+    #     print(f"Argument {i:>6}: {arg}") # should pass newsblur user,password and google api key #debug
+    #     print(" user : "  + str(sys.argv[1])) #debug
+    #     print(" pass : "  + str(sys.argv[2])) #debug        
+    try:
+        user_name = sys.argv[1]
+        password = sys.argv[2]
+    except IndexError:
+        print("Encountered Index Error in passed system arguments")
+        user_name =None
+        password = None
+    newsblur_object = api_calls.api_caller(user_name,password)
     if newsblur_object.login_newsblur() == True:
-    #   rss_feeds = newsblur_object.get_feeds()
       data_parser_object  = data_parser.Data_Parser() 
-      #   saved_stories = newsblur_object.get_saved_stories() # returns aggregated list of stories
       saved_stories_dataframe = data_parser_object.convert_to_dataframe(newsblur_object.get_saved_stories())
       csv_file = data_parser_object.data_frame_to_csv(saved_stories_dataframe)
       emailer = email_client.Emailer()

35852a551bf59ba9c2cc14553e4d99a70398aa8d
bored002 <miller.oleg@gmail.com>
2021-02-17 07:14:00 +0200

    disable basic data sciences

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import time
 import sys
 from src import api_calls
 from src import data_parser
+from src import data_processor
 from src import email_client
 def test_module():
     print("This is a Test.")
@@ -14,6 +15,7 @@ def test_module():
     print("Done waiting.")
 	
 if __name__ == "__main__":
+
     print(f"Arguments count: {len(sys.argv)}") #debug
     # for i, arg in enumerate(sys.argv):
     #     print(f"Argument {i:>6}: {arg}") # should pass newsblur user,password and google api key #debug
@@ -26,12 +28,16 @@ if __name__ == "__main__":
         print("Encountered Index Error in passed system arguments")
         user_name =None
         password = None
-    newsblur_object = api_calls.api_caller(user_name,password)
+    newsblur_object = api_calls.api_caller('alwaysgood1','Mfeed20')
     if newsblur_object.login_newsblur() == True:
       data_parser_object  = data_parser.Data_Parser() 
+      data_sciences = data_processor.data_science()
       saved_stories_dataframe = data_parser_object.convert_to_dataframe(newsblur_object.get_saved_stories())
-      csv_file = data_parser_object.data_frame_to_csv(saved_stories_dataframe)
-      emailer = email_client.Emailer()
-      mail_sender = emailer.email_csv(csv_file)
+      aggregation_dataframe = data_sciences.get_origin_distribution(saved_stories_dataframe)
+      data_parser_object.data_frame_to_csv(saved_stories_dataframe, 'saved_stories')
+      data_parser_object.data_frame_to_csv(aggregation_dataframe,'origin_distribution_aggregation', 'origin')
+
+    #   emailer = email_client.Emailer()
+    #   mail_sender = emailer.email_csv(stories_csv)
 #   test_module()
  #Place Holder  to mark EOF

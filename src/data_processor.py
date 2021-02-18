d783d3d69183fcf29411681fbb86342030bb068f
bored002 <miller.oleg@gmail.com>
2021-02-18 11:19:39 +0200

    Inital brnch Commit: Added histogram method

--- src/data_processor.py
+++ src/data_processor.py
@@ -1,7 +1,7 @@
 import pandas
 import seaborn
 import matplotlib
-
+import matplotlib.pyplot as plt
 class data_science(object):
     '''
     This module will handle various data analysis actions
@@ -18,12 +18,20 @@ class data_science(object):
         '''
         Generates a Data Frame which conaints the story distribution per origin website (feed)
         '''
-        grouped_data = raw_data_frame.groupby('origin').size()
+        grouped_data = raw_data_frame.groupby('origin').size() #TODO make sure the name of the columns are : Feed , Number of Saved Stories
         return grouped_data
 
     def update_distribution():
         '''
         '''
+
+    def visualise_historgram(self,data_frame, indexs_list):
+        '''
+        Generates a histogram graph using from a data frame based on the provided indexes
+        '''
+        #TODO Add legends to be more interperative
+        seaborn.displot(data_frame, binwidth = 3)
+        plt.show()
 
     def analyze_trends():
         '''

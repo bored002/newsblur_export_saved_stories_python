fe26a2d8cc41d0d809032503730b3721a266f2a1
bored002 <miller.oleg@gmail.com>
2021-09-16 15:50:22 +0300

    Adding log file to handle all information
    regarding debugging

---
+++ src/log_files.py
@@ -0,0 +1,15 @@
+import logging
+
+
+class logging_mechanism():
+    '''
+    '''
+
+    def __hash__(self) -> int:
+        pass
+
+    def write_to_log(self,message):
+        '''
+        Method to write to log file.
+        and to console if required
+        '''

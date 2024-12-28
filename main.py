e1db8c75003271ff78653573c14b4d0ad03c41ec
Miller Oleg <miller.oleg@gmail.com>
2024-12-28 09:25:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -57,9 +57,10 @@ if __name__ == "__main__":
     print('==========================================================================')
     #TODO:
     # Add plot for : network graph --> stories saved by feed
-    # Add plot/widget for Delta of stories saved
+    # Add plot/widget for Delta of stories saved, total stories, stories by feed
     # Add plot/widget for Sankey Diagram
-    #   emailer = email_client.Emailer()
+    
+#   emailer = email_client.Emailer()
     #   mail_sender = emailer.email_csv(stories_csv)
 
 #Place Holder  to mark EOF

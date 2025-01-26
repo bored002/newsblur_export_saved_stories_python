dee92f232793fcc50c2a12c56902f98cef2b15a4
Miller Oleg <miller.oleg@gmail.com>
2025-01-26 08:47:19 +0200

    Update generate_graphics.py
    adding update_Readme

--- src/generate_graphics.py
+++ src/generate_graphics.py
@@ -1,1 +1,18 @@
+import datetime
+class GeneratePage(object):
 
+
+  @staticmethod
+  def update_readme():
+    with open('README.md', 'w') as f:
+        f.write(f"""# My Project
+          ## Current Performance
+          
+          ![Gauge](/gauge.png)
+          
+          Last updated: {datetime.now()}
+          """)
+          
+# if __name__ == "__main__":
+#   pass
+  # update_readme()

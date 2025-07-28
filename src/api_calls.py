d279ff7586692ac0d9a985dcf51638e21957c1a0
Miller Oleg <miller.oleg@gmail.com>
2025-07-28 18:45:33 +0300

    Fixed upload, increase chunk to 1000

--- src/api_calls.py
+++ src/api_calls.py
@@ -288,7 +288,7 @@ class api_caller(object):
     except json.JSONDecodeError as json_err:
         print(f"Failed to decode JSON response: {json_err} - Content: {response.text}")
     
-    if i <= 300: # and i >= 400:
+    if i <= 1000:
         print(f"breaking the loop:::'get_starred_stories_by_hashes':: Returning all rtrieved stories with total count: {len(all_retrieved_stories)}")
         return all_retrieved_stories
         break
